799e2c4cd1665ab0e745e95840411185903b13ae
Lainezz <diegolinaresmojacar@gmail.com>
2022-09-22 20:07:18 +0200

    diej

--- PlantillaFolder/logica.js
+++ PlantillaFolder/logica.js
@@ -1,6 +1,5 @@
-function onIniciar(){
-    console.log("Uso esta funcion para inicializar elementos de la pagina");
-    
+function onIniciar() {
+  console.log("Uso esta funcion para inicializar elementos de la pagina");
 }
 
 window.onload = onIniciar();
