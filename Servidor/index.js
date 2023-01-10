8debcf24a071e75afb3a4bf8e0f1ad40836bac78
Lainezz <diegolinaresmojacar@gmail.com>
2023-01-10 09:16:26 +0100

    adicion de aclaraciones

--- Servidor/index.js
+++ Servidor/index.js
@@ -3,6 +3,13 @@ const express = require("express") //Tenemos la biblioteca cargada
 const app = express() //Aquí tenemos una aplicación "express"
 
 //VAMOS A VER LO QUE ES UNA FUNCIÓN MIDDLEWARE
+
+//El primer ejemplo de middleware es el más sencillo.
+//En este ejemplo se "bindea" una función a un método HTTP -> EN ESTE CASO CONCRETO, SE BINDEA A TODAS LAS PETICIONES
+//Las funciones middleware de express en su uso más común reciben por parámetros request, response y next
+//request: es la HTTP request, es decir, la petición http.
+//response: es la respuesta que da el servidor al navegador
+//next: es la siguiente función middleware que se ejecutaría
 app.use((req, res, next) => {
         console.log("Se ha realizado una petición")
         console.log("Metodo utilizado", req.method)
