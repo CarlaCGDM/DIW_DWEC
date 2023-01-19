f8505800902e79adc6be39b335b27c0c17f8c82d
Lainezz <diegolinaresmojacar@gmail.com>
2023-01-19 21:23:26 +0100

    adicion de codigo de clase

--- ServidorExpress/index.js
+++ ServidorExpress/index.js
@@ -24,32 +24,21 @@ app.use(
   }
 );
 
-app.get("/" , (req, res, next)=>{
-  console.log(`\x1b[34m%s\x1b[0m`, `[GET] Dentro de peticion GET`)
-  res.sendFile("/views_ejerc/index.html", {root: __dirname})
+app.get("/", (req, res, next)=>{
+  console.log(`\x1b[34m%s\x1b[0m`, `[GET] Dentro de peticion GET`);
+  res.sendFile(__dirname+"/views/index.html")
 })
 
-app.get("/logica.js", (req, res, next) => {
-  console.log(`\x1b[34m%s\x1b[0m`, `[GET] Dentro de peticion GET`);
-  res.sendFile("/views_ejerc/logica.js", { root: __dirname });
-});
-
-app.get("/styles.css", (req, res, next) => {
-  console.log(`\x1b[34m%s\x1b[0m`, `[GET] Dentro de peticion GET`);
-  res.sendFile("/views_ejerc/styles.css", { root: __dirname });
-});
-
-app.get("/:pj", (req,res,next)=>{
-
-  const urlParam = req.params.pj
+app.get("/:pj", (req, res, next) => {
+  let personaje = req.params.pj
   
-  let personajeADevolver = datosArr.find(personaje => personaje[0]===urlParam)
-
-  res.send(`<h1>${personajeADevolver[0]}</h1></br><h2>${personajeADevolver[1].blurb}</h2></br><h3>${personajeADevolver[1].tags[0]}</h3>`)
-})
-
-
-
+  let personajeData = datosArr.find( pers => pers[0] === personaje)
+  console.log(personajeData)
+  
+  res.send(
+    `<h1>${personajeData[0]}</h1><h2>${personajeData[1].blurb}</h2><h3>${personajeData[1].tags[0]}</h3>`
+  );
+});
 
 app.post("/", (req, res, next) => {
   console.log(`\x1b[34m%s\x1b[0m`, `[POST] Dentro de peticion POST`);
